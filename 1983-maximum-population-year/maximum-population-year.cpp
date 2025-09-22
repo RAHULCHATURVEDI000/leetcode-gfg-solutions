e197d086edfa63291e759e7327fc890bb0a2c8e1
RAHULCHATURVEDI000 <144182517+RAHULCHATURVEDI000@users.noreply.github.com>
2025-09-23 01:51:13 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (76.31%) - LeetSync

---
+++ 1983-maximum-population-year/maximum-population-year.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maximumPopulation(vector<vector<int>>& logs) {
+        int offset=1950;
+        vector<int> change(101,0);
+         for(const auto  &person:logs){
+            int birthyear=person[0];
+            int deathyear=person[1];
+
+             change[birthyear-offset]++;
+               change[deathyear-offset]--;
+
+         }
+         int mpop=0;
+         int cpop=0;
+         int eyear=0;
+         for(int i=0;i<change.size();i++){
+            cpop+=change[i];
+            if(cpop>mpop){
+                mpop=cpop;
+                eyear=i+offset;
+            }
+         }
+       
+       return eyear;
+
+    }
+};
